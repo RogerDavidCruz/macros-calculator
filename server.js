3cf03a53e25cda3829a6f4d833c12278b8db5e5e
Roger David Cruz <roger.david.cruz@gmail.com>
2021-10-19 21:40:30 -0400

    changing port variable

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
-const PORT = process.env.PORT || 3000
 
 //.env file inside config folder
 require("dotenv").config({path: "./config/.env"});
